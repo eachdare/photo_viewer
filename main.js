9b5a7c9a489928088964ecafaf667849f2a4565e
eachdare <eachdare@gmail.com>
2015-08-19 17:56:08 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var userObj ={uids:18756852};
-VK.Api.call('photos.getProfile',userObj ,function(photoArray){
-        console.log(photoArray);
-        $("<div class='dinamic'></div>");
-
-
-
-});
-
